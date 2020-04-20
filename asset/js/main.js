0790b9abc2e9cc2b6ae1c234e6013dbc3410965c
Muhammad ilham <muhilham0603@gmail.com>
2020-04-20 22:54:29 +0700

    javascript

---
+++ asset/js/main.js
@@ -0,0 +1,6 @@
+function navBurger(){
+	var burger = document.getElementById("ini-burger");
+	var menu = document.getElementById("navbarBasicExample");
+	burger.classList.toggle("is-active");
+	menu.classList.toggle("is-active");
+}
