48c61b0d4c7f69167968aef07275db81da831159
Muhammad ilham <muhilham0603@gmail.com>
2020-05-08 12:27:09 +0700

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,3 +4,44 @@ function navBurger(){
 	burger.classList.toggle("is-active");
 	menu.classList.toggle("is-active");
 }
+const app = new Vue({
+  el: '#app',
+  data: {
+    errors: [],
+    name: null,
+    email: null,
+    tel: null,
+    movie: null
+  },
+  methods: {
+    checkForm: function (e) {
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push("Name required.");
+      }
+      if (!this.email) {
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
+      if (!this.tel) {
+        this.errors.push("phone number required.");
+      } else if (this.tel.length < 6) {
+        this.errors.push('Valid phone number required.');
+      } else if (this.tel.length > 13) {
+        this.errors.push('Valid phone number required.');
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+})
